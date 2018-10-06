e049fc06e420f1937191271b16ea0fe891c2d484
Emilio Betancourt <embeor@gmail.com>
2018-10-06 20:17:12 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,6 +1,7 @@
 def my_each(argument)# put argument(s) here
-  counter = 0
-  while argument.length > counter
+  i = 0
+  while argument.length > i
+    yield argument[i]
     counter +=1
   end
 end
